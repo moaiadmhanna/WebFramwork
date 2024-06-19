3689381166e9cc37570d7501cd9f820fb08b6558
muayad <moaiadmhana999@gmail.comm>
2024-06-19 23:52:59 +0200

    added the db connection

--- Server/db.js
+++ Server/db.js
@@ -1,104 +1,112 @@
 const mongoose = require('mongoose');
 const User = require('./schemas/user');
+const Highscore = require('./schemas/highscore');
 
 const dbUser = 'root';
 const dbPassword = 'root';
 const dbName = 'highscores';
 
-(async function() {
-  try {
-    // Connect to MongoDB with authentication
-    await mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
+try{ 
+    mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
       useUnifiedTopology: true,
-      authSource: 'admin', // Change to your auth database if different
+      authSource: 'admin',
       user: dbUser,
       pass: dbPassword
     });
     console.log("Connected to MongoDB!");
-
-    const Users = await User.find({});
-    console.log(Users);
-  } catch (err) {
+  } 
+  catch (err) {
     console.error('Connection error:', err);
-  } finally {
-    mongoose.connection.close();
   }
-})();
 
 
-// /** in memory db */
-// const passwordHash = require('password-hash');
-// var randomToken = require('random-token');
-// var db = {
+/** in memory db */
+const passwordHash = require('password-hash');
+var randomToken = require('random-token');
+var db = {
+  
+    tokens: [],
 
-//     users: [
-//         { username: "test@test.at", password: passwordHash.generate("12345678") },
-//         { username: "linus@kernel.org", password: passwordHash.generate("abcdefg") },
-//         { username: "steve@apple.com", password: passwordHash.generate("123456") },
-//         { username: "bill@microsoft.com", password: passwordHash.generate("987654") }
-//     ],
-    
-//     tokens: [],
-    
-//     highscores: [ 
-//         { username: "test@test.at", score: 1600 },
-//         { username: "linus@kernel.org", score: 1900 },
-//         { username: "bill@microsoft.com", score: 400 }
-//     ],
+    signup: async function(username, password) {
+      try{
+        let user = await User.findOne({username : username})
+        if(user !== undefined){
+          return false
+        }
+        let newUser = new User({
+          username,
+          password : passwordHash.generate(password)
+        });
+        try {
+          await newUser.save();
+          } catch (err) {
+          return false
+          }
+        return true;
+      }
+      catch{
+        return false
+      }
+    },
 
-//     signup: function(username, password) {
-//         let user = this.users.find(u => u.username === username);
-//         if (user !== undefined) {
-//             return false;
-//         }
+    login: async function(username, password) {
+      try {
+        let user = await User.findOne({username: username});
+        if (user != undefined && passwordHash.verify(password, user.password)) {
+          let cred = this.tokens.find(cred => cred.username == username)
+          if(cred){
+              return cred
+          }       
+          return this.generateToken(user.username);
+        }
+        return null
+        } 
+        catch (err) {
+          return null
+        } 
+    },
 
-//         this.users.push({ username: username, password: passwordHash.generate(password)});
-//         return true;
-//     },
+    generateToken(username){
+        let token = randomToken(64);
+        let cred = {
+            username : username,
+            token : token
+        }
+        this.tokens.push(cred);
+        return cred;
+    },
 
-//     login: function(username, password) {
-//         let user = this.users.find(u => u.username === username);
-//         if (user != undefined && passwordHash.verify(password, user.password)) {
-//             let cred = this.tokens.find(cred => cred.username == username)
-//             if(cred){
-//                 return cred
-//             }       
-//             return this.generateToken(user.username);
-//         } 
+    deleteToken(authToken)  {
+        this.tokens = this.tokens.filter(e => e.token != authToken);
+    },
 
-//         return null;    
-//     },
+    isAuthenticated: function(authToken) {
+        return this.tokens.find(auth => auth.token == authToken) != undefined;
+    },
 
-//     generateToken(username){
-//         let token = randomToken(64);
-//         let cred = {
-//             username : username,
-//             token : token
-//         }
-//         this.tokens.push(cred);
-//         return cred;
-//     },
+    getAuthUser: function(authToken) {
+        return this.tokens.find(auth => auth.token == authToken);
+    },    
 
-//     deleteToken(authToken)  {
-//         this.tokens = this.tokens.filter(e => e.token != authToken);
-//     },
+    getHighscores: async function() {
+        let highscores = await Highscore.find();
+        return highscores.sort(function(a,b) { return b.score - a.score });
+    },
 
-//     isAuthenticated: function(authToken) {
-//         return this.tokens.find(auth => auth.token == authToken) != undefined;
-//     },
-
-//     getAuthUser: function(authToken) {
-//         return this.tokens.find(auth => auth.token == authToken);
-//     },    
-
-//     getHighscores: function() {
-//         return this.highscores.sort(function(a,b) { return b.score - a.score });
-//     },
-
-//     addHighscore: function(username, score) {
-//         this.highscores.push({ username: username, score: score });
-//     }
-// }
+    addHighscore: async function(username, score) {
+        let newScore = new Highscore({
+          username,
+          score,
+        })
+        try {
+          await newScore.save();
+          return true;
+          } 
+        catch (err) {
+          return false;
+          }
+    }
+}
 
 
-// module.exports = db;
+module.exports = db;
